ad9ccd35d67d49b46a5377ee77e78094ae74e31d
Colton Rhodenbaugh <coltonrhodenbaugh@Coltons-Air.hsd1.il.comcast.net>
2022-11-03 17:29:20 -0500

    Work is complete

--- src/index.js
+++ src/index.js
@@ -67,3 +67,26 @@ topContent.children[0].children[0].textContent = siteContent['main-content']['fe
 topContent.children[0].children[1].textContent = siteContent['main-content']['features-content']
 topContent.children[1].children[0].textContent = siteContent['main-content']['about-h4']
 topContent.children[1].children[1].textContent = siteContent['main-content']['about-content']
+
+//bottom
+const bottomContent = document.querySelector('.bottom-content')
+const h4sBottom = bottomContent.querySelectorAll('h4')
+h4sBottom[0].textContent = siteContent['main-content']['services-h4']
+h4sBottom[1].textContent = siteContent['main-content']['product-h4']
+h4sBottom[2].textContent = siteContent['main-content']['vision-h4']
+const psBottom = bottomContent.querySelectorAll('p')
+psBottom[0].textContent = siteContent['main-content']['services-content']
+psBottom[1].textContent = siteContent['main-content']['product-content']
+psBottom[2].textContent = siteContent['main-content']['vision-content']
+
+//cta
+document.querySelector('.cta .cta-text h1').textContent = siteContent.cta.h1
+document.querySelector('.cta .cta-text button').textContent = siteContent.cta.button
+
+//nav 
+const navLinks = document.querySelectorAll(' header nav a')
+const navLinksTexts = Object.values(siteContent.nav)
+navLinks.forEach((link,idx) => {
+  link.textContent = navLinksTexts[idx]
+  link.classList.add('italic')
+})
